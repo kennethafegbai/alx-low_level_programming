130e93cf734e97d6f4f2a59f51b9d68a43268321
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-12 16:26:51 -0700

    add file 2 in c ifelse dir

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	for (char i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
