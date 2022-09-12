b24f9a889c8967fa3dfca394a3a59323b36f1458
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-12 14:55:35 -0700

    update file 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
 	return (1);
 }
