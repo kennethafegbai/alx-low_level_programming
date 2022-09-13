c917585f7d2a0bfd9a24c00d95c345840001b302
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-12 17:12:39 -0700

    add file 4 in c ifelse dir

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Alwsys return 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e' || i == 'q')
+		{
+			continue;
+		}
+		putchar(i);
+	}
+	putchar('\n');
+}
