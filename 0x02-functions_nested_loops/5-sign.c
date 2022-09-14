aa22adcde45e48dcfed0d0b8a0d79debaad23675
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-14 01:37:38 -0700

    add task 5 of nested function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * int_print_sign - returns sign of int
+ * @n: character to check
+ * Return: return 1 if n > 0, return -1 if n < 1, return 0 if n == 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
