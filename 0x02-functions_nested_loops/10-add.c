439f0a652abd52b7b67a50433192e41bac8bdd96
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-14 06:24:29 -0700

    add task 10 of nested functions

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add -sum two integeres
+ * @num1: first integer
+ * @num2: second integer
+ * Return: return sum value
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int num);
 int print_last_digit(int num);
+int add(int num1, int num2);
