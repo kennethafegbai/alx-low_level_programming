9a4f5075a5ed90dda8d4ef5297daa0715d4fc287
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-14 05:21:59 -0700

    add task 7 of nested function

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @num: parameter value
+ * Return: returns last digit
+ */
+
+int print_last_digit(int num)
+{
+	if (num)
+	{
+	int result;
+
+	result = num % 10;
+	return (result);
+	}
+
+}
