f3d49c54747b742b630cd3dd82820f63928404b6
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-13 21:07:40 -0700

    corret betty errors

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,14 @@
  * print_alphabet_x10 - display alphabets 10 times
  */
 
-void print_alphabet_x10(void){
+void print_alphabet_x10(void)
+{
 	int i = 0;
 	char l;
 
 	while (i <= 9)
 	{
-	        for (l = 'a'; l <= 'z'; l++)
+		for (l = 'a'; l <= 'z'; l++)
 		{
 			_putchar(l);
 		}
