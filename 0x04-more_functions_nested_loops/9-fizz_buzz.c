72992d460e7e9583caa089b71ad907d7ceeab514
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-16 11:42:42 +0100

    add task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -print fizz, buzz, fizzbuzz in between numbers to 100
+ */
+
+void main(void)
+{
+
+for (int i = 1; i <= 100; i++)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+		printf("FizzBuzz");
+	else if (i % 3 == 0)
+		printf("Fizz");
+	else if (i % 5 == 0)
+		printf("Buzz");
+	else
+		printf("%d", i);
+	if (i == 100)
+		break;
+	printf(" ");
+}
+printf("\n");
+}
