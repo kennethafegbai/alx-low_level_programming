2d25ccb083f1141ae20c1a9cd100688b84666aed
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-16 03:38:24 +0100

    add task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - print line in terminal
+ * @n: paramenter value
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
