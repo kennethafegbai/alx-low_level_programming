e5fcaae9702cfb9657aa79afe219a67211e7ffe8
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-16 04:41:42 +0100

    add task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line
+ * @n: parameter value (int)
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (i == j)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
