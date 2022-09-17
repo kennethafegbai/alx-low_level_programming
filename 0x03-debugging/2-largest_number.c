2914d089589819f241562ae1c2e91a0f7f4af6e1
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-09-17 03:45:35 +0100

    fix another bug in task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,11 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if ((a > b && b > c) || (a > b && c > b && c < a))
+if ((a >= b && a >= c))
 {
 largest = a;
 }
-else if ((b > a && a > c) || (b > a && c > a && c < b))
+else if ((b >= a && b >= c)
 {
 largest = b;
 }
