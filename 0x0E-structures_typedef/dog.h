2ccf1dadf8af225f2e013d00a11a6d8912e74f38
Kenneth Afegbai <kennethafegbai@gmail.com>
2022-10-11 05:04:25 +0100

    add all tasks

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog -structure
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ */
+typedef struct dog{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+#endif
